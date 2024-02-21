d8f0e17da2bfaa90eaae9d24050a2b3e5c3b31d1
Marr <marat.salah1313@gmail.com>
2024-02-21 10:13:34 +0100

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-function App() {
-  return null;
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StartingPage from './pages/StartingPage';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
