224586297b1e54a8d0bf36e4bd8f9de5c88f217c
Marr <marat.salah1313@gmail.com>
2024-02-23 18:11:47 +0100

    add stop animation in second instruction

--- src/pages/SecondInstructionPage.jsx
+++ src/pages/SecondInstructionPage.jsx
@@ -1,10 +1,46 @@
 import styles from './SecondInstructionPage.module.scss';
 
+import { useState, useRef, useEffect } from 'react';
+
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
 const SecondInctructionPage = () => {
+  const [alienIsStopped, setAlienIsStopped] = useState(false);
+
+  const container = useRef();
+
+  useGSAP(() => {
+    gsap.to(container.current, {
+      y: '20%', // Move the element   100% of its width to the left
+      duration: 0.5, // Duration of the animation
+      repeat: -1, // Repeat the animation indefinitely
+      yoyo: true, // Reverse the animation each cycle
+      ease: 'power1.inOut', // Easing function for smooth animation
+    });
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (alienIsStopped === false) {
+        setAlienIsStopped(true);
+      } else {
+        setAlienIsStopped(false);
+      }
+    }, 500);
+
+    return () => clearInterval(intervalId);
+  });
+
   return (
     <div className={styles.startingPage}>
       <div className={styles.woodenBG}>
-        <div className={styles.alienBubble}></div>
+        {alienIsStopped === true ? (
+          <div className={styles.stoppedAlien}></div>
+        ) : (
+          <div className={styles.alien}></div>
+        )}
+        <div ref={container} className={styles.hand}></div>
         <div className={styles.instruction}></div>
         <button className={styles.startButton}>ПОГНАЛИ</button>
       </div>
