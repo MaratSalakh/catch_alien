be2ebcec7d8027b1ac774326be39d2ac279c85af
Marr <marat.salah1313@gmail.com>
2024-02-24 19:01:32 +0100

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartingPage from './pages/StartingPage';
 import FirstInctructionPage from './pages/FirstInstuctionPage';
 import SecondInctructionPage from './pages/SecondInstructionPage';
+import GamePage from './pages/GamePage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartingPage />} />
-        <Route path="/firstInstruction" element={<FirstInctructionPage />} />
-        <Route path="/secondInstruction" element={<SecondInctructionPage />} />
+        <Route path="/1" element={<FirstInctructionPage />} />
+        <Route path="/2" element={<SecondInctructionPage />} />
+        <Route path="/game" element={<GamePage />} />
       </Routes>
     </BrowserRouter>
   );
