a43ea5b81f11b6401e7f943008af42736bf80b9b
Marr <marat.salah1313@gmail.com>
2024-02-26 19:27:03 +0100

    add stopped state for alien

--- src/components/GoodAlien.jsx
+++ src/components/GoodAlien.jsx
@@ -1,9 +1,10 @@
 import styles from './GoodAlien.module.scss';
 
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { hideTextGood } from '../slices/aliensSlice';
+import { hideTextGood, addStop, removeStop } from '../slices/aliensSlice';
+
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 
@@ -16,22 +17,25 @@ export const GoodAlien = () => {
     (state) => state.aliens.entities.alienGood.textIsHide
   );
 
-  setTimeout(() => {
-    dispatch(hideTextGood());
-  }, 5000);
+  const alienIsStopped = useSelector(
+    (state) => state.aliens.entities.alienGood.isStopped
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch(hideTextGood());
+    }, 5000);
+  }, []);
 
   //goodAlien
   useGSAP(() => {
     const animate = () => {
       const element = goodAlien.current;
       if (element) {
-        const randomX = Math.random() * 300;
-        const randomY = Math.random() * 500;
-
         gsap.to(element, {
           duration: 4,
-          x: randomX,
-          y: randomY,
+          x: 'random(0, 300)',
+          y: 'random(0, 500)',
           delay: 2,
           ease: 'none',
           onComplete: animate, // Call the function again when the animation completes
@@ -42,11 +46,24 @@ export const GoodAlien = () => {
     animate();
   });
 
+  const alien =
+    alienIsStopped === true ? (
+      <div className={styles.stoppedAlien}></div>
+    ) : (
+      <div
+        onClick={() => {
+          dispatch(addStop());
+          setTimeout(() => dispatch(removeStop()), 4000);
+        }}
+        className={styles.goodAlien}></div>
+    );
+
   const text =
     textIsHide === true ? null : <div className={styles.textOfAlien}></div>;
+
   return (
     <div className={styles.alienWithText} ref={goodAlien}>
-      <div className={styles.goodAlien}></div>
+      {alien}
       {text}
     </div>
   );
