17d4d1a8a32c984009aec55f8c191f377657ec79
Marr <marat.salah1313@gmail.com>
2024-02-27 14:44:23 +0100

    add pages results and theend

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import StartingPage from './pages/StartingPage';
 import FirstInctructionPage from './pages/FirstInstuctionPage';
 import SecondInctructionPage from './pages/SecondInstructionPage';
 import GamePage from './pages/GamePage';
+import Results from './pages/Results';
+import TheEnd from './pages/TheEnd';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
         <Route path="/1" element={<FirstInctructionPage />} />
         <Route path="/2" element={<SecondInctructionPage />} />
         <Route path="/game" element={<GamePage />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/theend" element={<TheEnd />} />
       </Routes>
     </BrowserRouter>
   );
